7302831eca9902ecab03813c55012db573e6ae55
pablo <pablo.makoki@gmail.com>
2020-05-09 20:54:03 +0200

    Segundo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include "item.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    Item i;
+
+    i.setID(10000);
+    i.setNombre("MAKOKIS");
+    i.setPersistente(false);
+
+    cout << i.getID() << " " << i.getNombre() << endl;
+    if (i.esPersistente()) cout << "SI ES PERSISTENTE" << endl; else cout << "NO ES PERSISTENTE" << endl;
+
     return 0;
 }
