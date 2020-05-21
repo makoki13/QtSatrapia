6e0746686cce63eebc45567f6449efab55561926
pablo <pablo.makoki@gmail.com>
2020-05-22 00:50:10 +0200

    cosas

--- recursos/edificio.h
+++ recursos/edificio.h
@@ -1,9 +1,16 @@
 #ifndef EDIFICIO_H
 #define EDIFICIO_H
 
+#include "posicion.h"
+#include "almacen.h"
+#include "extractor.h"
 
 class Edificio
 {
+private:
+    Posicion pos;
+    Almacen almacen;
+    QList<Extractor> extractores;
 public:
     Edificio();
 };
